3cf0e9ad79346238c6e4cb1b3aaea5b02b4236fa
pedromesmer <pedro_mesmer@hotmail.com>
2020-08-15 21:13:10 -0300

    funções completas!

---
+++ Backup_do_SD.ino
@@ -0,0 +1,61 @@
+/*
+ * Cadastro e backup dos dados em um sd
+ * Pedro Mesmer, 2016
+ */
+ 
+#include <SD.h>
+#include <SPI.h>
+
+File myFile;
+
+char senha [10][4];
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+
+  if (!SD.begin(4)) {
+    Serial.println("SD não funcionou");
+    while (1) { 
+      // trava o programa! Verifique as conexões do SD e reinicie o dispositivo
+    }
+  } else {
+    Serial.println("SD funcionando");
+  }
+
+  // Define tudo como 0
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 4; j++) {
+      senha[i][j] = '0';
+    }
+  }
+  // Se NÃO existe esse arquivo no SD, cria arquivo zerado
+  if (!SD.exists("senha.txt")) {
+    myFile = SD.open("senha.txt", FILE_WRITE);
+    if (myFile) {
+      Serial.println("Criando arquivo 'senhas.txt' zerao no SD");
+      for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 4; j++) {
+          myFile.print(senha[i][j]);
+        }
+      }
+      myFile.close();
+    }
+  } else { // Se existe esse arquivo, faz o backup
+    myFile = SD.open("senha.txt", FILE_READ);
+    if (myFile) {
+      Serial.println("Salvando senhas do SD no programa");
+      for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 4; j++) {
+          senha[i][j] = myFile.read();
+        }
+      }
+      myFile.close();
+    }
+  }
+
+}
+
+void loop() {
+  
+}
